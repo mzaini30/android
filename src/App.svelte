0bcc3bcb3411e42f82b3e661f3e65e701e1b43bc
mzaini30 <muhzaini30@gmail.com>
2024-01-24 08:30:22 +0800

    upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script>
   import Router from "svelte-spa-router";
+  import Gerbang from "./kunci/src/Gerbang.svelte";
+  import data from "../android.json";
 
   const routes = {};
 </script>
 
-<Router {routes} />
+<Gerbang namaAplikasi={data.title}>
+  <Router {routes} />
+</Gerbang>
